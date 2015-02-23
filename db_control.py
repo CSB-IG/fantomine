302fdf5dc307b76abef9e04523c64a8cd844ccf9
Teran <ark8daniel@hotmail.com>
2015-02-22 23:42:09 -0600

    test de la base de datos

--- db_control.py
+++ db_control.py
@@ -27,7 +27,7 @@ class Db_Controller(threading.Thread):
     #Create the tables in the db, if they exists, then drop them
     def create_tables():
         t1 = "GENES"
-        t2 = "GENES__INTER"
+        t2 = "GENES_INTER"
         drop = "DROP TABLE IF EXISTS ?"
         self.cursor.execute(drop,(t1,))
         self.con.commit()
@@ -37,10 +37,12 @@ class Db_Controller(threading.Thread):
         table1 = "CREATE TABLE GENES (ID INTEGER PRIMARY KEY AUTOINCREMENT, GENE_ID TEXT NOT NULL,GENE_SYMBOL TEXT NOT NULL);"
         self.cursor.execute(table1)
         self.con.commit()
+				print "Se creo la tabla GENES correctamente"
         table2 = "CREATE TABLE GENES_INTER (ID INTEGER PRIMARY KEY AUTOINCREMENT, GENE1 INTEGER NOT NULL, GENE2 INTEGER NOT NULL, WEIGHT TEXT NOT NULL, FOREIGN KEY(GENE1) REFERENCES GENES(ID) ON DELETE CASCADE, FOREIGN KEY(GENE2) REFERENCES GENES(ID) ON DELETE CASCADE);"
         self.cursor.execute(table2)
         self.con.commit()
-                
+				print "Se creo la tabla GENES correctamente"        
+
     #Control data flow between the queues and the db
     def process_data():
         #first create tables 
